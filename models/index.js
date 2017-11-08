54d63b50296dd8fe5c76ca35b83c57fcc8d9a6fb
1657413883 <1657413883@qq.com>
2017-11-08 21:12:18 +0800

    feat: fix

--- models/index.js
+++ models/index.js
@@ -1,8 +1,9 @@
 var mongoose = require('mongoose');
 var config = require('../config.js');
 mongoose.Promise = global.Promise
+mongoose.connect(config.db, {useMongoClient:true});
 
-let db = mongoose.connect(config.db, {useMongoClient:true});
+var db = mongoose.connection;
 db.on('error', console.error.bind(console, '连接错误:'));
 
 // require models
