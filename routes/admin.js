532385701dc4493c86270ff532ab290944021421
1657413883 <1657413883@qq.com>
2017-11-11 21:24:09 +0800

    feat: 添加后台管理页面路由

--- routes/admin.js
+++ routes/admin.js
@@ -3,6 +3,25 @@ var router = express.Router();
 var adminController = require('../controllers/admin.js');
 
 // 后台管理首页
-router.get('/admin', adminController.showAdmin);
+router.get('/', adminController.showAdmin);
+
+
+// 后台文章列表页
+router.get('/articles', adminController.showAdminArticles);
+
+// 编辑
+router.get('/articles/edit/:id', function (req, res, next) {
+
+});
+
+// 编辑提交
+
+router.post('/articles/edit/:id', function (req, res, next) {
+
+});
+
+// 删除
+
+router.get('/articles/delete/:id', adminController.deleteArticle);
 
 module.exports = router;
